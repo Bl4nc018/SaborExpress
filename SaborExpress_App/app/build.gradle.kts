cf23ae4ebb7bf504de6ed91e2c125a3ddc338cb1
Bl4nc018 <abr04work@gmail.com>
2024-05-22 20:01:57 +0200

    Actualizados el gradle y el manifest por los permisos del Internet junto con algunas implementaciones.

--- SaborExpress_App/app/build.gradle.kts
+++ SaborExpress_App/app/build.gradle.kts
@@ -32,6 +32,17 @@ android {
 }
 
 dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.android.volley:volley:1.2.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 
     implementation(libs.appcompat)
     implementation(libs.material)
