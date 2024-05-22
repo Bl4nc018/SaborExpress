1454f637bbf0b7ad1d38f11f16b74ca58f18f2fa
Bl4nc018 <abr04work@gmail.com>
2024-05-22 21:14:25 +0200

    Actualizados los datos del Recycler para que coincidan con el json de ejemplo. Añadidos también dos layouts para mostrar tanto cada celda del recycler como el fragmento del mismo.

--- SaborExpress_App/app/src/main/java/com/example/saborexpress/recycler/RecyclerItems.java
+++ SaborExpress_App/app/src/main/java/com/example/saborexpress/recycler/RecyclerItems.java
@@ -1,5 +1,8 @@
 package com.example.saborexpress.recycler;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,20 +11,44 @@ public class RecyclerItems {
 
     // Variables para almacenar los datos de cada elemento
     private String recipe_name;
+    private String description;
+    private String food_type;
+    private String ingredients;
+    private String steps;
+    private String image_url;
 
 
     // Métodos getter para cada variable
     public String getRecipe_Name() { return recipe_name; }
+    public String getDescription() { return description; }
+    public String getFood_type() { return food_type; }
+    public String getIngredients() { return ingredients; }
+    public String getSteps() { return steps; }
+    public String getImage_url() { return image_url; }
 
     // Constructor que toma los datos como parámetros
-    public RecyclerItems(String place_name){
-        this.recipe_name=place_name;
+    public RecyclerItems(String recipe_name, String description, String food_type,
+                         String ingredients, String steps, String image_url){
+        this.recipe_name=recipe_name;
+        this.description=description;
+        this.food_type=food_type;
+        this.ingredients=ingredients;
+        this.steps=steps;
+        this.image_url=image_url;
     }
 
     // Constructor que toma un objeto JSON y extrae los datos
     public RecyclerItems(JSONObject json){
         try{
             this.recipe_name = json.getString("recipe_name");
+            this.description = json.getString("description");
+            this.food_type = json.getString("food_type");
+            this.ingredients = json.getString("ingredients");
+            this.steps = json.getString("steps");
+            this.image_url = json.getString("image_url");
         }catch (JSONException e){ e.printStackTrace(); }
     }
+
+
+
 }
--- SaborExpress_App/app/src/main/java/com/example/saborexpress/recycler/RecyclerViewHolder.java
+++ SaborExpress_App/app/src/main/java/com/example/saborexpress/recycler/RecyclerViewHolder.java
@@ -1,14 +1,10 @@
 package com.example.saborexpress.recycler;
 
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.saborexpress.R;
@@ -17,18 +13,32 @@ import com.example.saborexpress.R;
 public class RecyclerViewHolder extends RecyclerView.ViewHolder {
     // Variables para los elementos de la vista
     private TextView recipe_name;
+    private TextView description;
+    private TextView food_type;
+    private TextView ingredients;
+    private TextView steps;
+    private ImageView image_url;
 
     // Constructor del ViewHolder
     public RecyclerViewHolder(@NonNull View ivi){
         super(ivi);
         // Encontrar los elementos de la vista
-//        recipe_name = ivi.findViewById(R.id.recipe_name);
+        recipe_name = ivi.findViewById(R.id.recipe_name);
+        description = ivi.findViewById(R.id.description);
+//        food_type = ivi.findViewById(R.id.food_type);
+//        ingredients = ivi.findViewById(R.id.ingredients);
+//        steps = ivi.findViewById(R.id.steps);
+//        image_url = ivi.findViewById(R.id.image_url);
     }
 
     // Método para mostrar los datos en los elementos de la vista
     public void showData(RecyclerItems items){
         // Establecer el texto de los TextViews y la imagen del ImageView
         recipe_name.setText(items.getRecipe_Name());
+        description.setText(items.getDescription());
+        food_type.setText(items.getFood_type());
+        ingredients.setText(items.getIngredients());
+        steps.setText(items.getSteps());
 //        Util.downloadBitmapToImageView(items.getImage_url(), this.imageView);
     }
 }
